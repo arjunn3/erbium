f66e079d3d5e02bcfa3a5b329ea173511045d690
arjunn3 <arjunnagendra03@gmail.com>
2025-04-05 02:27:48 +0530

    Add files via upload

---
+++ HackerNewsServer/hackernews-server/src/index.ts
@@ -0,0 +1,7 @@
+import "dotenv/config"
+import { serve } from "@hono/node-server";
+import { allRoutes } from "./routes/routes.js";
+
+serve(allRoutes);
+
+console.log("server is running at http://localhost:3000/")
